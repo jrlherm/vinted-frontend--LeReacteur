eb2ff2f45eea3f92ad43132ee6c1b98269593050
JR Lherm <lherm.jr@gmail.com>
2023-08-23 16:50:41 +0200

    Working on the axios request of the offer page

--- src/page/Offer.jsx
+++ src/page/Offer.jsx
@@ -1,15 +1,52 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Offer = () => {
   const { id } = useParams();
-  console.log(id);
 
-  // Find the offer where the ID is equal to the params of the page.
-  const offerInfos = "aze";
+  const [offerData, setOfferData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://lereacteur-vinted-api.herokuapp.com/offers/:{id}`
+        );
+        setOfferData(response.data);
+        setIsLoading(false);
+        console.log("offerData ==>", offerData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <div className="container">
+      <p>Loading ...</p>
+    </div>
+  ) : (
     <div className="main">
-      <p>l'ID de cette offre est : {id}</p>
+      <div className="container">
+        <div className="offer-left">
+          <img src="#" alt="" className="offer-img" />
+        </div>
+        <div className="offer-right">
+          <p className="offer-price"></p>
+          <div className="offer-infos">
+            <div className="offer-brand"></div>
+            <div className="offer-size"></div>
+            <div className="offer-state"></div>
+            <div className="offer-color"></div>
+            <div className="offer-location"></div>
+          </div>
+        </div>
+        <p>l'ID de cette offre est : {id}</p>
+      </div>
     </div>
   );
 };
